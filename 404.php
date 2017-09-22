ce3ee45bab01ca458f1a5a30e2bc6eade71db9e2
Erick Olivares <erickolivares21@gmail.com>
2017-09-21 23:52:55 -0700

    added 1/3 column width

--- 404.php
+++ 404.php
@@ -1,6 +1,14 @@
-<?php get_header(); ?>
+<?php 
+
+get_header(); 
+
+?>
 
 <h1>404</h1>
 <p>The page you are trying to find does not exist.</p>
 
-<?php get_footer(); ?>
+<?php 
+
+get_footer(); 
+
+?>
